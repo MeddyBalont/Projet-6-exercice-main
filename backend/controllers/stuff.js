4f5702690a5391400ff7048445076f402e0364c4
MeddyBalont <Meddy.Balont@hotmail.com>
2023-12-08 13:20:31 +0100

    11e

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -2,17 +2,18 @@ const Thing = require('../models/thing');
 
 exports.createThing = (req, res, next) => {
   const thingObject = JSON.parse(req.body.thing);
-  delete thingObject._id;
-  delete thingObject._userId;
-  const thing = new Thing({
-      ...thingObject,
-      userId: req.auth.userId,
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-  });
+   delete thingObject._id;
+   delete thingObject._userId;
+   const thing = new Thing({
+       ...thingObject,
+       userId: req.auth.userId,
+       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+   });
+ 
+   thing.save()
+   .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
+   .catch(error => { res.status(400).json( { error })})
 
-  thing.save()
-  .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
-  .catch(error => { res.status(400).json( { error })})
 };
 
 exports.getOneThing = (req, res, next) => {
